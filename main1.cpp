edd20091eb82d468556bfc74fd6636e0513ebf43
Shardul Singh <41698288+shardul1999@users.noreply.github.com>
2021-06-17 20:41:20 +0530

    Add files via upload

---
+++ main1.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> func(int n) {
+         vector <int> vec(n);
+         int x = 0;
+         for(int i = 0; i < n - 1; i++){
+            vec[i] = (i + 1);
+            x += (i + 1);
+         }
+         vec[n-1] =-x;
+         return vec;
+}
+
+int32_t main()
+{
+
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+
+    {
+        int n;
+        cin>>n;
+        vector<int> v(func(n));
+        for(int i=0;i<v.size();i++)
+            cout<<v[i]<<" ";
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
